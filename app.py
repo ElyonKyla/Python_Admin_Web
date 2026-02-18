b8bd8db360123431bbf0a54fe0b46fc49b36f972
ElyonKyla <taniveiga@gmail.com>
2026-02-18 18:53:04 +0100

    Store registered users in memory using SimpleNamespace

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template, request
+from types import SimpleNamespace
 
 app = Flask(__name__)
+
+# Lista en memoria (por ahora)
+usuarios = []
 
 @app.route("/", methods=["GET", "POST"])
 def crear_usuario():
 
     if request.method == "POST":
-        email = request.form["email"]
-        nombre = request.form["nombre"]
-        apellidos = request.form["apellidos"]
-        password = request.form["password"]
-        telefono = request.form["telefono"]
-        edad = request.form["edad"]
+        usuario = SimpleNamespace(
+            email=request.form["email"],
+            nombre=request.form["nombre"],
+            apellidos=request.form["apellidos"],
+            password=request.form["password"],
+            telefono=request.form["telefono"],
+            edad=request.form["edad"]
+        )
+
+        usuarios.append(usuario)
 
         return f"""
-        <h2>Usuario recibido</h2>
-        <p>Email: {email}</p>
-        <p>Nombre: {nombre}</p>
-        <p>Apellidos: {apellidos}</p>
-        <p>Teléfono: {telefono}</p>
-        <p>Edad: {edad}</p>
+        <h2>Usuario guardado en memoria</h2>
+        <p>Total usuarios en memoria: {len(usuarios)}</p>
         <a href="/">Volver</a>
         """
 
