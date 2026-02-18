44894a6e31895e50e59870bf77f25cb570db55f8
ElyonKyla <taniveiga@gmail.com>
2026-02-18 19:04:45 +0100

    Implement JSON persistence for registered users

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from types import SimpleNamespace
+import json
+
 
 app = Flask(__name__)
 
@@ -21,13 +23,20 @@ def crear_usuario():
 
         usuarios.append(usuario)
 
+        # Convertimos objetos a diccionarios
+        usuarios_dict = [u.__dict__ for u in usuarios]
+
+        with open("usuarios.json", "w", encoding="utf-8") as f:
+            json.dump(usuarios_dict, f, indent=4)
+
         return f"""
-        <h2>Usuario guardado en memoria</h2>
+        <h2>Usuario guardado en JSON</h2>
         <p>Total usuarios en memoria: {len(usuarios)}</p>
         <a href="/">Volver</a>
         """
 
     return render_template("form.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
